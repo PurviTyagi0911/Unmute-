825f469e13021d1ddf317b2bb09c1d2dfb2d02db
Purvi Tyagi <pt8495044@gmail.com>
2026-06-25 18:29:17 +0530

    club name added, image in create posts added

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,session,redirect,url_for
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key='unmute_secret_key'
 def init_db():
     conn = sqlite3.connect("unmute.db")
     cursor = conn.cursor()
@@ -14,7 +15,9 @@ def init_db():
         location TEXT NOT NULL,
         date TEXT,
         time TEXT,
-        description TEXT
+        description TEXT,
+        club_name TEXT,
+        image_url TEXT
     )
     """)
 
@@ -23,8 +26,14 @@ def init_db():
 @app.route('/')
 def home():
     return render_template('home.html')
-@app.route('/clubs_login')
+@app.route('/clubs_login', methods=['GET', 'POST'])
 def club_login():
+   
+        
+    if request.method=='POST':
+        club_name=request.form['club_name']
+        session['club_name']=club_name
+        return redirect(url_for('create_posts'))
     return render_template('clubs_login.html')
 @app.route("/student_feed")
 def student_feed():
@@ -32,7 +41,7 @@ def student_feed():
     cursor = conn.cursor()
 
     cursor.execute("""
-    SELECT title, category, location,date,time,description
+    SELECT title, category, location,date,time,description,image_url,club_name
     FROM events
     """)
 
@@ -49,7 +58,10 @@ def student_feed():
             "location": row[2],
             'date':row[3],
             'time':row[4],
-            'description':row[5]
+            'description':row[5],
+            'club_name': row[6],
+            'image_url': row[7]
+
         })
 
     return render_template(
@@ -59,6 +71,8 @@ def student_feed():
 
 @app.route('/create_posts', methods=['GET', 'POST'])
 def create_posts():
+    if 'club_name' not in session:
+        return redirect(url_for('club_login'))
 
 
     if request.method == 'POST':
@@ -67,7 +81,9 @@ def create_posts():
       location = request.form['location']
       date = request.form['date']
       time = request.form['time']
-      description = request.form['description']
+      description = request.form.get('description')
+      club_name=session['club_name']
+      image_url=request.form.get('image_url')
 
 
 
@@ -76,13 +92,15 @@ def create_posts():
       cursor = conn.cursor()
 
       cursor.execute("""
-      INSERT INTO events(title, category, location,date,time,description)
-      VALUES (?, ?, ?,?,?,?)
-      """,(title, category, location,date,time,description)
+      INSERT INTO events(title, category, location,date,time,description,club_name,image_url)
+      VALUES (?, ?, ?,?,?,?,?,?)
+      """,(title, category, location,date,time,description,club_name,image_url)
                     )
 
       conn.commit()
       conn.close()
+
+      return redirect(url_for('student_feed'))
     return render_template('create_posts.html')
 
 if __name__ == "__main__":
