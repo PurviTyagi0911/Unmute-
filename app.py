81e62f869e3166629f148ed85b324950a4835338
Purvi Tyagi <pt8495044@gmail.com>
2026-06-30 00:28:42 +0530

    edit posts feature added

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def my_posts():
     cursor=conn.cursor()
     club_n=session['club_name']
     cursor.execute(
-        """SELECT title, category, location,date,time,description,image_url,club_name FROM events WHERE club_name=?
+        """SELECT id,title, category, location,date,time,description,image_url,club_name FROM events WHERE club_name=?
     """,(club_n,)
     )
     rows = cursor.fetchall()
@@ -82,14 +82,15 @@ def my_posts():
     events = []
     for row in rows:
         events.append({
-            "title": row[0],
-            "category": row[1],
-            "location": row[2],
-            'date':row[3],
-            'time':row[4],
-            'description':row[5],
-            'image_url': row[6],
-            'club_name': row[7]
+            "id": row[0],
+            'title':row[1],
+            "category": row[2],
+            "location": row[3],
+            'date':row[4],
+            'time':row[5],
+            'description':row[6],
+            'image_url': row[7],
+            'club_name': row[8]
         })
     return render_template('my_posts.html',events=events)
 
@@ -122,6 +123,53 @@ def create_posts():
 
       return redirect(url_for('club_dashboard'))
     return render_template('create_posts.html')
+@app.route('/edit_posts/<int:post_id>',methods=['POST','GET'])
+def edit_posts(post_id):
+    if 'club_name' not in session:
+        return(redirect(url_for('club_login')))
+    conn=sqlite3.connect('unmute.db')
+    cursor=conn.cursor()
+    club_n=session['club_name']
+    if request.method=='POST':
+     cursor.execute(
+        """UPDATE events SET title=?, category=?, location=?,date=?,time=?,description=?,image_url=? WHERE id=? AND club_name=?
+     """, (request.form["title"],
+            request.form["category"],
+            request.form["location"],
+            request.form["date"],
+            request.form["time"],
+            request.form["description"],
+            request.form["image_url"],
+            post_id,
+            session["club_name"])
+     )
+     conn.commit()
+     conn.close()
+     return redirect(url_for("my_posts"))
+
+    cursor.execute("""
+    SELECT title,category,location,date,time,description,image_url
+    FROM events
+    WHERE id=? AND club_name=?
+    """,(post_id,session["club_name"]))
+
+    row=cursor.fetchone()
+
+    conn.close()
+
+    event={
+        "title":row[0],
+        "category":row[1],
+        "location":row[2],
+        "date":row[3],
+        "time":row[4],
+        "description":row[5],
+        "image_url":row[6]
+    }
+
+    return render_template("edit_posts.html",event=event)
+    
+
 
 
 if __name__ == "__main__":
